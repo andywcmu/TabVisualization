2dd95c7063cd14fe65b9326a4fba418abf6c9b68
Andy Wang <eliowang@cmu-851378.wv.cc.cmu.edu>
2015-03-16 18:53:20 -0400

    basic tracking functionality works. Start cleaning up stuff.

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
+GOOGLE_SEARCH_STR = "http://www.google.com/search?q=";
+
 var counter = 0;
 
-/* url: {title, nodeIndex} */
+/* url: {title, index} */
 var sites = {};
 
 var nodes = [];
@@ -8,98 +10,161 @@ var labelAnchors = [];
 var labelAnchorLinks = [];
 var links = [];
 
+var tabStatus = [];
 
-/* tabID: nodeIndex */
-activeTab = {};
-
-current = {'tabId': -1, 'windowId': -1, 'url': null};
+var current = {'tabId': -1, 'nodeId': -1};
 
 function urlHash (url) {
-    return url;
+    return url.match(/^[^\#]+/)[0];
+}
+
+function getTab (tabId) {
+    ts = tabStatus.filter(function (t) {
+        return t.id == tabId;
+    });
+    
+    if (ts.length == 0) {
+        return null;
+    } else {
+        return ts[0];        
+    }
+}
+
+function updateCurrentTabID (tabId) {
+    current.tabId = tabId;
+}
+
+function updateCurrentNodeID (tabId) {
+    tab = getTab(tabId);
+
+    if (tab != null) {
+        url = urlHash(tab.url);
+        console.log(url);
+        current.nodeId = (url in sites) ? sites[url].index : -1;
+    }
+}
+
+DIA_ROOT_NODE = 30;
+DIA_CHILD_NODE = 16;
+
+function createNode (url, node_diameter) {
+    url = urlHash(url);
+    if (url in sites) {
+        node = nodes[sites[url].index];
+        if (node.size < node_diameter) {
+            node.size = node_diameter;
+        }
+    } else {
+        node = {
+            label : url,
+            url : url,
+            size : node_diameter
+        };
+
+        nodes.push(node);
+
+        for (var i = 0; i < 2; i++) {
+            labelAnchors.push({
+                node : node
+            });
+        }
+
+        labelAnchorLinks.push({
+            source : (nodes.length - 1) * 2,
+            target : (nodes.length - 1) * 2 + 1,
+            weight : 1
+        });
+        
+        sites[url] = {title : url,
+                      index : nodes.length - 1};
+    }
+}
+
+function createLink (parentUrl, childUrl) {
+    parentUrl = urlHash(parentUrl);
+    childUrl = urlHash(childUrl);
+
+    if ((parentUrl in sites) && (childUrl in sites)) {
+        console.log(sites[parentUrl].index + " " + sites[childUrl].index);
+        links.push({
+            source : sites[parentUrl].index,
+            target : sites[childUrl].index,
+            weight : 1
+        });
+    }
+}
+
+function createChildren (parentUrl, childUrl) {
+    createNode(childUrl, DIA_CHILD_NODE);
+    createLink(parentUrl, childUrl);
+}
+
+function createRoot (url) { 
+    createNode(url, DIA_ROOT_NODE);
+}
+
+
+LINK_IGNORE_TYPE = ["auto_subframe"];
+LINK_CHILD_TYPE = ["link", "form_submit"];
+
+function inLinkType (trans, typeList) {
+    for (var i = 0; i < typeList.length; i++) {
+        if (trans == typeList[i]) {
+            return true;
+        }
+    }
+
+    return false;
 }
 
 chrome.webNavigation.onCommitted.addListener(function (detail) {
 
-    // console.log(detail.transitionType);
+    if (!(inLinkType(detail.transitionType, LINK_IGNORE_TYPE))) {
+        if (inLinkType(detail.transitionType, LINK_CHILD_TYPE)) {
+            tab = getTab(current.tabId);
 
-    if (detail.transitionType != "auto_subframe") {
-        newUrl = urlHash(detail.url);
-
-
-        if (newUrl in sites) {
-            /* increase weight? */
-        } else {
-            var node = {
-                label : newUrl
-            };
-            nodes.push(node);
-            labelAnchors.push({
-                node : node
-            });
-            labelAnchors.push({
-                node : node
-            });
-            labelAnchorLinks.push({
-                source : (nodes.length - 1) * 2,
-                target : (nodes.length - 1) * 2 + 1,
-                weight : 1
-            });
-            
-            sites[newUrl] = {title : newUrl,
-                             index : nodes.length - 1};
-        }
-
-        if (detail.transitionType != 'auto_subframe') {
-            if (detail.transitionType == 'link') {
-                
-                
-
-                console.log("link from " + current.url + " to " + newUrl);
-                console.log("id " + sites[current.url].index + " -> " + sites[newUrl].index);
-
-                links.push({
-                    source : sites[current.url].index,
-                    target : sites[newUrl].index,
-                    weight : 1
-                });
-
-                // newTabId = detail.tabId;
-                // console.log('LINK from tab ' + current.tabId + ' to ' + newTabId);
-
+            if (tab == null) {
+                console.log("error: tab " + current.tabId + " cannot be found");
+            } else {
+                createChildren(tab.url, detail.url);
             }
-        }
-
-        current.url = newUrl;
-    }
+        } else {
+            createRoot(detail.url);
+        }   
     
+        chrome.tabs.query({}, function(tabs) {
+            tabStatus = tabs;
+            updateCurrentNodeID(current.tabId);
+        }); 
+    }
+
 });
+
+function updateLabel (url, title) {
+    url = urlHash(url);
+    if (url in sites) {
+        nodes[sites[url].index].label = title;
+    }
+}
 
 chrome.webNavigation.onDOMContentLoaded.addListener(function (detail) {
     chrome.tabs.get(detail.tabId, function (tab) {
-        /* update title of a tab */
-        url = urlHash(tab.url);
-        if (url in sites) {
-            nodes[sites[url].index].label = tab.title;
+        if (tab != null) {
+            updateLabel(tab.url, tab.title);
         }
     });
 });
 
-// chrome.windows.onCreated.addListener(function (wd) {
-//     console.log(wd);
-// });
 
-// chrome.webNavigation.onBeforeNavigate.addListener(function (details) {
-//     chrome.tabs.query({}, function (tabs) {
-//         console.log(tabs);
-//     });
-// });
+chrome.omnibox.onInputEntered.addListener(function (info) {
+    chrome.tabs.update({
+        url: GOOGLE_SEARCH_STR + info
+    });
+});
 
 chrome.tabs.onActivated.addListener(function (info) {
-    current.tabId = info.tabId;
-    current.windowId = info.windowId;
-    chrome.tabs.get(info.tabId, function (tab) {
-        current.url = urlHash(tab.url);
-    });
+    updateCurrentTabID(info.tabId);
+    updateCurrentNodeID(info.tabId);
 });
 
 chrome.windows.onFocusChanged.addListener(function (id) {
@@ -109,12 +174,9 @@ chrome.windows.onFocusChanged.addListener(function (id) {
     };
 
     chrome.tabs.query(queryInfo, function(tabs) {
-        current.tabId = tabs[0].id;
-        current.windowId = tabs[0].windowId;
-        chrome.tabs.get(tabs[0].id, function (tab) {
-            current.url = urlHash(tab.url);
-        });
+        tabId = tabs[0].id;
+        updateCurrentTabID(tabId);
+        updateCurrentNodeID(tabId);
     });
 
-    
 });
--- foreground.js
+++ foreground.js
@@ -42,9 +42,9 @@ var bg = chrome.extension.getBackgroundPage();
 
 // bg.counter++;
 
-var graphContainerW = 500, graphContainerH = 500;
+var graphContainerW = 800, graphContainerH = 600;
 
-var labelDistance = 0;
+// var labelDistance = 0;
 
 
 
@@ -62,21 +62,21 @@ var graph = graphContainer.append("g");
 
 
 
-var force = d3.layout.force().size([graphContainerW, graphContainerH]).nodes(bg.nodes).links(bg.links).gravity(1).linkDistance(50).charge(-3000).linkStrength(function(x) {
-    return x.weight * 10
+var force = d3.layout.force().size([graphContainerW, graphContainerH]).nodes(bg.nodes).links(bg.links).gravity(0.2).linkDistance(100).charge(-3000).linkStrength(function(x) {
+    return x.weight * 1
 });
 
 force.start();
 
-var force2 = d3.layout.force().nodes(bg.labelAnchors).links(bg.labelAnchorLinks).gravity(0).linkDistance(50).linkStrength(8).charge(-100).size([graphContainerW, graphContainerH]);
+var force2 = d3.layout.force().nodes(bg.labelAnchors).links(bg.labelAnchorLinks).gravity(0).linkDistance(25).linkStrength(8).charge(-100).size([graphContainerW, graphContainerH]);
 force2.start();
 
 
 
-var link = graph.selectAll("line.link").data(bg.links).enter().append("svg:line").attr("class", "link").style("stroke", "#CCC");
+var link = graph.selectAll("line.link").data(bg.links).enter().append("svg:line").attr("class", "link").style("stroke", "#FFFFFF").style("stroke-width", 3);
 
 var node = graph.selectAll("g.node").data(force.nodes()).enter().append("svg:g").attr("class", "node");
-node.append("svg:circle").attr("r", 16).style("fill", "#555").style("stroke", "#FFF").style("stroke-width", 3);
+node.append("svg:circle").attr("r", function(d, i){return bg.nodes[i].size;}).style("fill", function(d, i){return bg.current.nodeId == i ? "#A3D900" : "#EEEEEE";}).style("stroke", "#FFFFFF").style("stroke-width", 3);
 
 var anchorLink = graph.selectAll("line.anchorLink").data(bg.labelAnchorLinks)//.enter().append("svg:line").attr("class", "anchorLink").style("stroke", "#999");
 
@@ -86,22 +86,111 @@ anchorNode.append("svg:text").text(function(d, i) {
     return i % 2 == 0 ? "" : d.node.label
 }).style("fill", "#555").style("font-family", "Arial").style("font-size", 12);
 
+
+function urlHash (url) {
+    return url.match(/^[^\#]+/)[0];
+}
+
+function centerNode(n) {
+    scale = zoomListener.scale();
+    x = -n.x;
+    y = -n.y;
+    x = x * scale + graphContainerW / 2;
+    y = y * scale + graphContainerH / 2;
+    d3.select('g').transition()
+        // .duration(duration)
+        .attr("transform", "translate(" + x + "," + y + ")scale(" + scale + ")");
+    zoomListener.scale(scale);
+    zoomListener.translate([x, y]);
+}
+
+node.on("click", function(d, i){
+    chrome.tabs.create({url: bg.nodes[i].url});
+    // centerNode(d);
+    /* Should check if the url already exists in one of the tabs first
+     * if yes, switch to that tab. otherwise open a new tab with that url.
+     * Work on this later!
+     */
+    // chrome.tabs.query({}, function (tabs){
+    //     for (var j = 0; j < tabs.length; j++) {
+    //         chrome.tabs.get(tabs[j].id, function (tab) {
+    //             console.log(urlHash(bg.nodes[i].url));
+    //             if ("https://instagram.com" == urlHash(bg.nodes[i].url)) {
+    //                 chrome.tabs.highlight(tab.id);
+    //                 console.log("HIGHLIGHT "+tab.id);
+    //             }
+    //         });
+    //     }
+    // });
+});
+
+// var currentNode = graph.selectAll("g.node")[bg.current.nodeId];
+// centerNode(currentNode);
+
+
+
+
+// var updateLink = function() {
+//     this.attr("x1", function(d) {
+//         return d.source.x;
+//     }).attr("y1", function(d) {
+//         return d.source.y;
+//     }).attr("x2", function(d) {
+//         return d.target.x;
+//     }).attr("y2", function(d) {
+//         return d.target.y;
+//     });
+
+// }
+
 var updateLink = function() {
     this.attr("x1", function(d) {
-        return d.source.x;
+        // return d.source.x - currentNodeX + (graphContainerW / 2);
+        return getRelatedTranslateX(d.source.x);
     }).attr("y1", function(d) {
-        return d.source.y;
+        return getRelatedTranslateY(d.source.y);
+        // return d.source.y - currentNodeY + (graphContainerH / 2);
     }).attr("x2", function(d) {
-        return d.target.x;
+        return getRelatedTranslateX(d.target.x);
+        // return d.target.x - currentNodeX + (graphContainerW / 2);
     }).attr("y2", function(d) {
-        return d.target.y;
+        return getRelatedTranslateY(d.target.y);
+        // return d.target.y - currentNodeY + (graphContainerH / 2);
     });
+}
 
+
+
+var currentNodeX = graphContainerW / 2;
+var currentNodeY = graphContainerH / 2;
+
+// function getRelatedTranslate (x, y) {
+//     relatedX = x - currentNodeX + (graphContainerW / 2);
+//     relatedY = y - currentNodeY + (graphContainerH / 2);
+    
+//     return "translate(" + relatedX + "," + relatedY + ")";
+// }
+
+function getRelatedTranslateX (x) {
+    return x - currentNodeX + (graphContainerW / 2);
+}
+
+function getRelatedTranslateY (y) {
+    return y - currentNodeY + (graphContainerH / 2);
 }
 
 var updateNode = function() {
-    this.attr("transform", function(d) {
-        return "translate(" + d.x + "," + d.y + ")";
+    this.attr("transform", function(d, i) {
+        return "translate(" + getRelatedTranslateX(d.x) + "," + getRelatedTranslateY(d.y) + ")";        
+
+
+        // return getRelatedTranslate(d.x, d.y);
+
+        // relatedX = d.x - currentNodeX + (graphContainerW / 2);
+        // relatedY = d.y - currentNodeY + (graphContainerH / 2);
+
+        // return "translate(" + d.x + "," + d.y + ")";
+        // return "translate(" + relatedX + "," + relatedY + ")";
     });
 
 }
@@ -109,7 +198,16 @@ var updateNode = function() {
 
 force.on("tick", function() {
 
+    console.log("" + currentNodeX + "  " + currentNodeY);
+
     force2.start();
+
+    node.each(function(d, i) {
+        if (i == bg.current.nodeId) {
+            currentNodeX = d.x;
+            currentNodeY = d.y;
+        }
+    });
 
     node.call(updateNode);
 
@@ -129,6 +227,8 @@ force.on("tick", function() {
             shiftX = Math.max(-b.width, Math.min(0, shiftX));
             var shiftY = 5;
             this.childNodes[1].setAttribute("transform", "translate(" + shiftX + "," + shiftY + ")");
+            // this.childNodes[1].setAttribute("transform", getRelatedTranslate(shiftX, shiftY));
+
         }
     });
 
@@ -137,5 +237,4 @@ force.on("tick", function() {
 
     link.call(updateLink);
     anchorLink.call(updateLink);
-
 });
